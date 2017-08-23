f21b7151c2a596d00c6b18223140e9fcec93ece1
dmn <moldmn@gmail.com>
2017-08-23 13:28:06 +0300

    undo spawn

--- src/connection/mc_worker.erl
+++ src/connection/mc_worker.erl
@@ -48,7 +48,7 @@ init(Options) ->
   NextReqFun = mc_utils:get_value(next_req_fun, Options, fun() -> ok end),
   mc_auth:auth(Socket, Login, Password, ConnState#conn_state.database, NetModule),
 
-  Storage = ets:new(storage, [set, public, {keypos, 1}]),
+  Storage = ets:new(storage, [set, private, {keypos, 1}]),
   erlang:send_after(1000, self(), size),
   gen_server:enter_loop(?MODULE, [],
     #state{socket = Socket, conn_state = ConnState, net_module = NetModule, next_req_fun = NextReqFun, request_storage = Storage})
@@ -91,9 +91,7 @@ handle_cast(_, State) ->
 handle_info({Net, _Socket, Data}, State = #state{request_storage = RequestStorage}) when Net =:= tcp; Net =:= ssl ->
   Buffer = <<(State#state.buffer)/binary, Data/binary>>,
   {Responses, Pending} = mc_worker_logic:decode_responses(Buffer),
-  spawn(fun()->
-    mc_worker_logic:process_responses(Responses, RequestStorage)
-  end),
+  mc_worker_logic:process_responses(Responses, RequestStorage),
   {noreply, State#state{buffer = Pending}};
 handle_info({NetR, _Socket}, State) when NetR =:= tcp_closed; NetR =:= ssl_closed ->
   {stop, tcp_closed, State};
